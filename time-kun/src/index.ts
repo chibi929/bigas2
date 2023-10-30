bbbca612b9109ffb582245702048c919d234c31b
chibi929 <ibihc929@gmail.com>
2023-10-30 21:34:17 +0900

    [Update] Support split time-kun.

--- time-kun/src/index.ts
+++ time-kun/src/index.ts
@@ -9,9 +9,16 @@ function myFunction(): void {
   const fileHeadSuffixes = PropertiesService.getScriptProperties().getProperty('FILE_HEAD_SUFFIXES') || ''
 
   const fileName = `${Const.YYYY}年${Const.M}月${fileSuffix}`
-  fileHeadSuffixes?.split(',').forEach((fileHeadSuffix) => {
+
+  // 入力用
+  const copiedFileIdForInput = copyByFileId(copiedFrom!, '入力用', fileName)
+  updateSpreadSheet(copiedFileIdForInput)
+
+  // 参照用
+  fileHeadSuffixes?.split(',').forEach((fileHeadSuffix, idx) => {
     const copiedFileId = copyByFileId(copiedFrom!, fileHeadSuffix, fileName)
     updateSpreadSheet(copiedFileId)
+    updateRefSheet(copiedFileId, copiedFileIdForInput, idx === 0 ? 'I8:K' : 'O8:Q')
   })
 }
 
@@ -49,5 +56,24 @@ function updateSpreadSheet(id: string): void {
         sheet.deleteRow(rowNum)
       }
     })
+  })
+}
+
+function updateRefSheet(id: string, refId: string, refRange): void {
+  const ss = SpreadsheetApp.openById(id)
+  const sheets = ss.getSheets()
+  sheets.forEach((sheet) => {
+    const endOfMonthRow = (() => {
+      for (const endOfMonthRow of Const.END_OF_MONTH_ROWS) {
+        if (sheet.getRange(`A${endOfMonthRow}`).getValue()) {
+          return endOfMonthRow
+        }
+      }
+    })()
+
+    sheet.deleteColumns(9, 19)
+    sheet.getRange(`C8:E${endOfMonthRow}`).clearContent()
+    const sheetName = sheet.getSheetName()
+    sheet.getRange('C8').setValue(`=IMPORTRANGE("${refId}", "${sheetName}!${refRange}${endOfMonthRow}")`)
   })
 }
